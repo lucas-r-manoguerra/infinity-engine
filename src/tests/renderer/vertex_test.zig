403a4cd4418a20340227a8fbd5f3fa1f0c2d94c8
Lucas R Manoguerra <developer.spoonacular.004@gmail.com>
2026-07-31 11:18:38 -0300

    chore: remove legacy Zig codebase (C++23 migration)

--- src/tests/renderer/vertex_test.zig
+++
@@ -1,8 +0,0 @@
-const std = @import("std");
-const testing = std.testing;
-const Vertex = @import("../../renderer/renderer.zig").Vertex;
-
-test "Vertex size with normals is 40 bytes" {
-    // Vertex = x,y,z,nx,ny,nz,u,v,inv_w (9 x f32 @ 4 = 36) + Color (packed u32, 4) = 40 bytes
-    try testing.expectEqual(@as(usize, 40), @sizeOf(Vertex));
-}
